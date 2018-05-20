d687a68bb3cfdd47d4b16527e6b1fcd4c4a2feba
pandaxuan <pandaxuan518@gmail.com>
2018-05-21 02:56:42 +0800

    Add files via upload

---
+++ sleepman.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRStandardAssets.Examples;
+using System.IO;
+using System.Text;
+
+public class sleepman : MonoBehaviour {
+    private Animator anim;
+    private double t, t1;
+    private int t2;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        t2 = slide.interaction;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        t = Time.timeSinceLevelLoad;
+        t1 = slide.scoree0 + 30;
+        if (slide.scoree0 > 0 && t < t1)
+        {
+            anim.SetBool("sleep", true);
+            if(ExampleInteractiveItem.collidename == "Male1" && slide.interaction == t2)
+            {
+                Debug.Log("sleeppppppppppp");
+                slide.interaction++;
+                StreamWriter sw = new StreamWriter("../test2222222/Assets/Resources/LogData5.csv", true);
+                StringBuilder sb = new StringBuilder();
+                sb.Append(slide.interaction).Append(',').Append(this.gameObject.name);
+                sw.WriteLine(sb);
+                sw.Flush();
+                sw.Close();
+            }
+        }
+        else
+        {
+            anim.SetBool("sleep", false);
+        }
+	}
+}
---
+++ timer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class timer : MonoBehaviour {
+    public Text timeLabel;
+    private float time = 839.0f;
+    public GameObject obj;
+    SteamVR_TrackedObject trackedObj;
+    // Use this for initialization
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        time -= Time.deltaTime;
+        var minutes = time / 60;
+        var seconds = time % 60;
+        var fraction = (time * 100) % 100;
+
+        timeLabel.text = string.Format("{0:00} : {1:00} : {2:000}", minutes, seconds, fraction);
+
+	}
+}
